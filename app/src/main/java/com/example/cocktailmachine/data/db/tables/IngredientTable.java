4fbb628b5b73d8992e3e804a0d9c1d9faf63461a
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-01-10 12:02:42 +0100

    Update IngredientTable.java

--- app/src/main/java/com/example/cocktailmachine/data/db/tables/IngredientTable.java
+++ app/src/main/java/com/example/cocktailmachine/data/db/tables/IngredientTable.java
@@ -20,17 +20,17 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
         public static final String TABLE_NAME = "Ingredient";
         public static final String COLUMN_NAME_NAME = "Name";
         public static final String COLUMN_NAME_ALCOHOLIC = "alcoholic";
-        public static final String COLUMN_NAME_AVAILABLE = "available";
-        public static final String COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS = "FluidPumpedInMilliseconds";
-        public static final String COLUMN_NAME_PUMP_ID = "PumpID";
+        //public static final String COLUMN_NAME_AVAILABLE = "available";
+        //public static final String COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS = "FluidPumpedInMilliseconds";
+        //public static final String COLUMN_NAME_PUMP_ID = "PumpID";
         public static final String COLUMN_NAME_COLOR = "Color";
 
         public static final String COLUMN_TYPE_ID = TYPE_ID;
         public static final String COLUMN_TYPE_NAME = TYPE_TEXT;
         public static final String COLUMN_TYPE_ALCOHOLIC = TYPE_BOOLEAN;
-        public static final String COLUMN_TYPE_AVAILABLE = TYPE_BOOLEAN;
-        public static final String COLUMN_TYPE_FLUID_PUMPED_IN_MILLISECONDS = TYPE_INTEGER;
-        public static final String COLUMN_TYPE_PUMP_ID= TYPE_LONG;
+        //public static final String COLUMN_TYPE_AVAILABLE = TYPE_BOOLEAN;
+        //public static final String COLUMN_TYPE_FLUID_PUMPED_IN_MILLISECONDS = TYPE_INTEGER;
+        //public static final String COLUMN_TYPE_PUMP_ID= TYPE_LONG;
         public static final String COLUMN_TYPE_COLOR = TYPE_INTEGER;
 
         IngredientTable(){
@@ -48,9 +48,9 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
             columns.add(this._ID);
             columns.add(COLUMN_NAME_NAME);
             columns.add(COLUMN_NAME_ALCOHOLIC);
-            columns.add(COLUMN_NAME_AVAILABLE);
-            columns.add(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS);
-            columns.add(COLUMN_NAME_PUMP_ID);
+            //columns.add(COLUMN_NAME_AVAILABLE);
+            //columns.add(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS);
+            //columns.add(COLUMN_NAME_PUMP_ID);
             columns.add(COLUMN_NAME_COLOR);
             return columns;
         }
@@ -61,9 +61,9 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
             columns.add(COLUMN_TYPE_ID);
             columns.add(COLUMN_TYPE_NAME);
             columns.add(COLUMN_TYPE_ALCOHOLIC);
-            columns.add(COLUMN_TYPE_AVAILABLE);
-            columns.add(COLUMN_TYPE_FLUID_PUMPED_IN_MILLISECONDS);
-            columns.add(COLUMN_TYPE_PUMP_ID);
+            //columns.add(COLUMN_TYPE_AVAILABLE);
+            //columns.add(COLUMN_TYPE_FLUID_PUMPED_IN_MILLISECONDS);
+            //columns.add(COLUMN_TYPE_PUMP_ID);
             columns.add(COLUMN_TYPE_COLOR);
             return columns;
         }
@@ -74,9 +74,9 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
             String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_NAME));
             boolean alcoholic = 0 < cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_ALCOHOLIC));
             boolean available = 0 < cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_AVAILABLE));
-            int fluid = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS));
-            long pump_id = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_NAME_PUMP_ID));
-            int color = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_COLOR));
+            //int fluid = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS));
+            //long pump_id = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_NAME_PUMP_ID));
+            //int color = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_COLOR));
             return new SQLIngredient(id, name,
                     alcoholic, available, fluid, pump_id, color);
         }
@@ -86,9 +86,9 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
             ContentValues cv = new ContentValues();
             cv.put(COLUMN_NAME_NAME, element.getName());
             cv.put(COLUMN_NAME_ALCOHOLIC, element.isAlcoholic());
-            cv.put(COLUMN_NAME_AVAILABLE, element.isAvailable());
-            cv.put(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS, element.getFluidInMilliliter());
-            cv.put(COLUMN_NAME_PUMP_ID, element.getPump().getID());
+            //cv.put(COLUMN_NAME_AVAILABLE, element.isAvailable());
+            //cv.put(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS, element.getFillLevel());
+            //cv.put(COLUMN_NAME_PUMP_ID, element.getPump().getID());
             cv.put(COLUMN_NAME_COLOR, element.getColor());
             return cv;
         }
@@ -102,15 +102,4 @@ public class IngredientTable extends BasicColumn<SQLIngredient> {
             }
         }
 
-        public void setEmptyPumps(SQLiteDatabase db){
-            ContentValues cv = new ContentValues();
-            cv.put(COLUMN_NAME_AVAILABLE, false);
-            cv.put(COLUMN_NAME_FLUID_PUMPED_IN_MILLISECONDS, -1);
-            cv.put(COLUMN_NAME_PUMP_ID, -1L);
-            try {
-                this.updateColumnToConstant(db, cv);
-            } catch (NoSuchColumnException e) {
-                e.printStackTrace();
-            }
-        }
 }
