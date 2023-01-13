805342d4c6d30df1001b3b0174e1fd36e7c546b9
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-01-13 12:49:52 +0100

    Update TestDB.java
    add image url

--- app/src/test/java/com/example/cocktailmachine/data/db/TestDB.java
+++ app/src/test/java/com/example/cocktailmachine/data/db/TestDB.java
@@ -33,7 +33,7 @@ public class TestDB {
     }
 
     @Test
-    public void add_topics(){
+    public void recipe_add_topics(){
         Recipe recipe = Recipe.makeNew("Magarita_topics");
         recipe.addOrUpdate(Topic.makeNew("Eis2", "gefrorenes Wasser, 3 Würfel"));
         Recipe db_recipe = NewDatabaseConnection.getDataBase().getRecipe(recipe.getID());
@@ -43,18 +43,45 @@ public class TestDB {
 
     @Test
     public void new_ingredient(){
-        Ingredient ingredient = Ingredient.makeNew("Kokosmilch", true, 67);
+        Ingredient ingredient = Ingredient.makeNew("Kokosmilch", false, 67);
         Ingredient db_ingredient = NewDatabaseConnection.getDataBase().getIngredient(ingredient.getID());
         assert ingredient.equals(db_ingredient);
     }
 
     @Test
-    public void add_ingredient(){
-        Recipe recipe = Recipe.makeNew("Magarita_topics");
-        recipe.addOrUpdate(Ingredient.makeNew("Wodka", true, 123),3);
+    public void ingredient_add_image_url(){
+        Ingredient ingredient = Ingredient.makeNew("Pfeffi", true, 46);
+        ingredient.addImageUrl("test_ingredient.png");
+        Ingredient db_ingredient = NewDatabaseConnection.getDataBase().getIngredient(ingredient.getID());
+        assert ingredient.equals(db_ingredient);
+    }
+
+    @Test
+    public void recipe_add_ingredient(){
+        Recipe recipe = Recipe.makeNew("Magarita_ingredient");
+        recipe.addOrUpdate(
+                Ingredient.makeNew("Wodka", true, 123),
+                3);
         Recipe db_recipe = NewDatabaseConnection.getDataBase().getRecipe(recipe.getID());
         assert recipe.equals(db_recipe);
     }
 
+    @Test
+    public void recipe_add_ingredient_with_id(){
+        Recipe recipe = Recipe.makeNew("Magarita_ingredient_id");
+        recipe.addOrUpdate(
+                Ingredient.makeNew("Grapefruit-Saft", false, 324).getID(),
+                7);
+        Recipe db_recipe = NewDatabaseConnection.getDataBase().getRecipe(recipe.getID());
+        assert recipe.equals(db_recipe);
+    }
+
+    @Test
+    public void recipe_add_imageUrl(){
+        Recipe recipe = Recipe.makeNew("Magarita_url");
+        recipe.addOrUpdate("test.png");
+        Recipe db_recipe = NewDatabaseConnection.getDataBase().getRecipe(recipe.getID());
+        assert recipe.equals(db_recipe);
+    }
 
 }
