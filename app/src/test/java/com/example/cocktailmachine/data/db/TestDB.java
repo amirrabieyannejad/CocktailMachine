235ee2ac5cf5c7af8f5c02e4f9ba7abf7ba4d682
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-01-13 12:04:18 +0100

    Update TestDB.java

--- app/src/test/java/com/example/cocktailmachine/data/db/TestDB.java
+++ app/src/test/java/com/example/cocktailmachine/data/db/TestDB.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.cocktailmachine.data.Recipe;
+
 public class TestDB {
     @Test
     public void addition_isCorrect() {
@@ -12,5 +14,12 @@ public class TestDB {
     @Test
     public void connection(){
         assertNotNull("DB", NewDatabaseConnection.getDataBase());
+    }
+
+    @Test
+    public void new_recipe(){
+        Recipe recipe = Recipe.makeNew("Magarita");
+        Recipe db_recipe = NewDatabaseConnection.getDataBase().getRecipe(recipe.getID());
+        assert recipe.equals(db_recipe);
     }
 }
