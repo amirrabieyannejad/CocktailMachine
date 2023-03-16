be4b4f22246ce0a84d13f2ea3e43a01a87afe32d
Freya Dorn <hekate@fastmail.com>
2023-03-16 21:43:34 +0100

    turn ble debug script into proper test suite

--- esp32/test_bluetooth.py
+++ esp32/test_bluetooth.py
@@ -13,6 +13,20 @@ import time
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s', datefmt='%y-%m-%d %H:%M:%S')
 
+async def read_all_chars(client):
+  logging.info("reading all chars...")
+
+  for service in client.services:
+    logging.info(f"  service: {service}")
+
+    for char in service.characteristics:
+      if "read" in char.properties:
+        try:
+          value = bytes(await client.read_gatt_char(char.uuid))
+          logging.info(f"  - read: {char.uuid} -> {value})")
+        except Exception as e:
+          logging.error(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {e}")
+
 async def main():
   parser = argparse.ArgumentParser(
     prog = "Bluetooth LE reader",
@@ -27,17 +41,7 @@ async def main():
     while True:
       logging.info("--- scan started ---")
       async with BleakClient(args.address) as client:
-        print("Services:")
-        for service in client.services:
-          logging.info(f"service: {service}")
-
-          for char in service.characteristics:
-            if "read" in char.properties:
-              try:
-                value = bytes(await client.read_gatt_char(char.uuid))
-                logging.info(f"read: {char.uuid} -> {value})")
-              except Exception as e:
-                logging.error(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {e}")
+        await read_all_chars(client)
 
       logging.info("--- scan complete ---")
       time.sleep(args.sleep)
