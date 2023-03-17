27bc8707b6b09f2f348860c4457db6745eb890cd
Freya Dorn <hekate@fastmail.com>
2023-03-17 03:47:04 +0100

    send a test command and wait for a response

--- esp32/test_bluetooth.py
+++ esp32/test_bluetooth.py
@@ -6,10 +6,22 @@ from bleak import BleakClient
 # utilities
 import argparse
 import asyncio
+import json
 import logging
 import platform
 import sys
 import time
+
+UUID_STATUS         	= "0f7742d4-ea2d-43c1-9b98-bb4186be905d"
+UUID_STATUS_BASE    	= "c0605c38-3f94-33f6-ace6-7a5504544a80"
+UUID_STATUS_STATE   	= "e9e4b3f2-fd3f-3b76-8688-088a0671843a"
+UUID_STATUS_LIQUIDS 	= "fc60afb0-2b00-3af2-877a-69ae6815ca2f"
+UUID_STATUS_RECIPES 	= "9ede6e03-f89b-3e52-bb15-5c6c72605f6c"
+UUID_STATUS_COCKTAIL	= "7344136f-c552-3efc-b04f-a43793f16d43"
+UUID_COMM           	= "dad995d1-f228-38ec-8b0f-593953973406"
+UUID_COMM_USER      	= "eb61e31a-f00b-335f-ad14-d654aac8353d"
+UUID_COMM_ADMIN     	= "41044979-6a5d-36be-b9f1-d4d49e3f5b73"
+UUID_NOTIFY         	= "00002902-0000-1000-8000-00805f9b34fb"
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s', datefmt='%y-%m-%d %H:%M:%S')
 
@@ -27,6 +39,25 @@ async def read_all_chars(client):
         except Exception as e:
           logging.error(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {e}")
 
+async def comm_msg(client, uuid, message):
+  notification_received = asyncio.Event()
+  def notification_handler(_sender, _data):
+    notification_received.set()
+
+  await client.start_notify(uuid, notification_handler)
+
+  logging.info(f"message: {message.decode('utf8')}")
+  await client.write_gatt_char(uuid, message)
+
+  await notification_received.wait()
+  value = bytes(await client.read_gatt_char(uuid))
+  logging.info(f"-> response: {value.decode('utf8')}")
+
+  await client.stop_notify(uuid)  # stop notifications
+
+def j(obj):
+  return json.dumps(obj, ensure_ascii=False).encode('utf8')
+
 async def main():
   parser = argparse.ArgumentParser(
     prog = "Bluetooth LE reader",
@@ -41,6 +72,8 @@ async def main():
     while True:
       logging.info("--- scan started ---")
       async with BleakClient(args.address) as client:
+        await comm_msg(client, UUID_COMM_USER, j({"cmd": "test"}))
+
         await read_all_chars(client)
 
       logging.info("--- scan complete ---")
