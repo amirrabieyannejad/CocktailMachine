cc5240174bad26f38252e20f7d6b8d666b8eedfa
Wieberp <Wieberp@students.uni-marburg.de>
2023-03-31 15:14:33 +0200

    Zwischenergebnisse

--- app/src/main/java/com/example/cocktailmachine/logic/BildgeneratorGlas.java
+++ app/src/main/java/com/example/cocktailmachine/logic/BildgeneratorGlas.java
@@ -45,7 +45,7 @@ public class BildgeneratorGlas {
         glasHintereDarstellung.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
         glasHintereDarstellung.draw(canvas);
 
-        canvas = new BildgeneratorGlas().erzeugeFlüssigkeitGlas(context,canvas,recipe);
+        canvas = new BildgeneratorGlas().erzeugeFlüssigkeitGlas(context,canvas,recipe,(float)1);
 
         Drawable glasFordereDarstellung =  ResourcesCompat.getDrawable(res, R.drawable.glas_fordere_darstellung, null);
         glasFordereDarstellung.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -74,42 +74,77 @@ public class BildgeneratorGlas {
         return (bm);
     }
 
+    public static Bitmap bildgenerationGlas(Context context, Recipe recipe,Float filling) throws TooManyTimesSettedIngredientEcxception, NoSuchIngredientSettedException {
 
-    private Canvas erzeugeFlüssigkeitGlas(Context context, Canvas canvas, Recipe recipe) throws TooManyTimesSettedIngredientEcxception, NoSuchIngredientSettedException {
+        Bitmap bm = Bitmap.createBitmap(800,800,Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bm);
         Resources res = context.getResources();
 
+        Drawable glasHintereDarstellung =  ResourcesCompat.getDrawable(res, R.drawable.glas_hintere_darstellung, null);
+        glasHintereDarstellung.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        glasHintereDarstellung.draw(canvas);
 
+        if (filling > 1){
+            filling = (float)1;
+        }
+        if (filling < 0){
+            filling = (float)0;
+        }
+
+
+        canvas = new BildgeneratorGlas().erzeugeFlüssigkeitGlas(context,canvas,recipe,filling);
+
+        Drawable glasFordereDarstellung =  ResourcesCompat.getDrawable(res, R.drawable.glas_fordere_darstellung, null);
+        glasFordereDarstellung.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        glasFordereDarstellung.draw(canvas);
+
+        Drawable myImage2 = ResourcesCompat.getDrawable(res, R.drawable.glas_fluessigkeit02, null);
+
+
+        Bitmap bm1 = BitmapFactory.decodeResource(res,R.drawable.glas_fluessigkeit01);
+        Bitmap bm2 = BitmapFactory.decodeResource(res,R.drawable.glas_fluessigkeit02);
+
+
+
+
+
+        /**myImage.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+
+         int color = Color.RED;
+         ColorFilter colorfilter = new PorterDuffColorFilter(0, PorterDuff.Mode.SRC_ATOP);
+         myImage.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+         myImage.draw(canvas);
+         myImage2.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+         //myImage.setTint(0xFFFF0000);
+         myImage2.draw(canvas);**/
+
+        return (bm);
+    }
+
+
+    private Canvas erzeugeFlüssigkeitGlas(Context context, Canvas canvas, Recipe recipe,Float filling) throws TooManyTimesSettedIngredientEcxception, NoSuchIngredientSettedException {
+
+        if (filling > 1){
+            filling = (float)1;
+        }
+        if (filling < 0){
+            filling = (float)0;
+        }
+
+        Resources res = context.getResources();
         List<Ingredient> ingredientList = recipe.getIngredients();
-        int sumLiquit = 0;
         int animationSlots= listIdGlasFlüssigkeit.length;
         int slotCounter= 0;
 
         Map<Ingredient,Integer> proportionenFlüssigkeitenImGlas =
                 this.getProportionOfLiquidInCocktail(recipe,  ingredientList, animationSlots);
 
-        /**for (Ingredient ingredient : recipe.getIngredients()){
-
-            //recipe.getSpecificIngredientVolume(ingredient);
-            sumLiquit += recipe.getSpecificIngredientPumpTime(ingredient);
-        }*/
-
-
-        /**for (Ingredient ingredient : ingredientList){
-            int numberSlots = this.getNumberOfSlots(sumLiquit,animationSlots-slotCounter,recipe, ingredient);
-            for (int i = 0 ; i < numberSlots; i++){
-                Drawable myImage =  ResourcesCompat.getDrawable(res, listIdGlasFlüssigkeit[i+slotCounter], null);
-                myImage.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-                myImage.setColorFilter(ingredient.getColor(), PorterDuff.Mode.MULTIPLY);//MULTIPLY,SRC_IN,SRC_ATOP
-                myImage.draw(canvas);
-            }
-            slotCounter += numberSlots;
-            sumLiquit -= recipe.getSpecificIngredientPumpTime(ingredient);
-        }*/
-
-
         for (Ingredient ingredient : ingredientList){
             int numberSlots = proportionenFlüssigkeitenImGlas.get(ingredient);
             for (int i = 0 ; i < numberSlots; i++){
+                if((slotCounter+(i+1))/animationSlots>filling){
+                    return(canvas);
+                }
                 Drawable myImage =  ResourcesCompat.getDrawable(res, listIdGlasFlüssigkeit[i+slotCounter], null);
                 myImage.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
                 myImage.setColorFilter(ingredient.getColor(), PorterDuff.Mode.MULTIPLY);//MULTIPLY,SRC_IN,SRC_ATOP
@@ -150,6 +185,7 @@ public class BildgeneratorGlas {
     private Map<Ingredient,Integer> getProportionOfLiquidInCocktail(Recipe recipe, List<Ingredient> ingredientList,int animationSlots) throws TooManyTimesSettedIngredientEcxception, NoSuchIngredientSettedException {
         int sumLiquit = 0;
         int slotCounter= 0;
+        List<Ingredient> newIngredientList = this.sortIngredientsAscending(recipe,ingredientList);
 
         Map<Ingredient,Integer> outputMap = new HashMap<>();
 
@@ -157,7 +193,7 @@ public class BildgeneratorGlas {
             sumLiquit += recipe.getSpecificIngredientPumpTime(ingredient);
         }
 
-        for (Ingredient ingredient : ingredientList){
+        for (Ingredient ingredient : newIngredientList){
             int numberSlots = this.getNumberOfSlots(sumLiquit,animationSlots-slotCounter,recipe, ingredient);
             outputMap.put(ingredient,numberSlots);
             slotCounter += numberSlots;
