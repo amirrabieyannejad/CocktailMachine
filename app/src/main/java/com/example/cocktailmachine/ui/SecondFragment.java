86d247fb687518b3bc9e06469fe6c11e61255680
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-04-14 16:05:28 +0200

    binding

--- app/src/main/java/com/example/cocktailmachine/ui/SecondFragment.java
+++ app/src/main/java/com/example/cocktailmachine/ui/SecondFragment.java
@@ -9,7 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.cocktailmachine.ui.databinding.FragmentSecondBinding;
+import com.example.cocktailmachine.R;
+import com.example.cocktailmachine.databinding.FragmentSecondBinding;
+
 
 public class SecondFragment extends Fragment {
 
