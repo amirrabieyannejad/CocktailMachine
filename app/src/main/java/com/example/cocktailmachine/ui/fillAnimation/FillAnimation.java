e815771f75d5a4bc816e15e5c3c72f364f1ce8ee
Wieberp <Wieberp@students.uni-marburg.de>
2023-05-18 22:14:21 +0200

    Erweiterung der füllanimation von Cocktails

--- app/src/main/java/com/example/cocktailmachine/ui/fillAnimation/FillAnimation.java
+++ app/src/main/java/com/example/cocktailmachine/ui/fillAnimation/FillAnimation.java
@@ -1,4 +1,143 @@
 package com.example.cocktailmachine.ui.fillAnimation;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.cocktailmachine.R;
+import com.example.cocktailmachine.SingeltonTestdata;
+import com.example.cocktailmachine.data.Orientation;
+import com.example.cocktailmachine.data.Recipe;
+import com.example.cocktailmachine.data.db.DatabaseConnection;
+import com.example.cocktailmachine.data.db.NotInitializedDBException;
+import com.example.cocktailmachine.logic.FlingAnalysis;
+import com.example.cocktailmachine.ui.singleCocktailChoice.SingleCocktailChoice;
+import com.example.cocktailmachine.ui.singleCocktailChoice.fragment1;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class FillAnimation {
+
+    private GestureDetector mDetector;
+    private int counter = 0;
+    private int fragmentCounter = 0;
+
+
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+
+    SingeltonTestdata singeltonCocktail = SingeltonTestdata.getSingelton();
+    List<Recipe> recipes = new LinkedList();
+
+
+
+    List<String> testData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_cocktail_choice);
+
+        testData = new LinkedList<>();
+        testData.add("a");
+        testData.add("b");
+        testData.add("c");
+
+
+        recipes.add(singeltonCocktail.getRecipe());
+        recipes.add(singeltonCocktail.getRecipe2());
+
+        //load List of available recipes
+       /* try {
+            this.recipes = loadRecipes(this);
+        } catch (NotInitializedDBException e) {
+            e.printStackTrace();
+        }*/
+
+        //initialise Fragment Manager
+        fragmentManager = getSupportFragmentManager();
+
+
+        fragment1 f1 = fragment1.newInstance();
+        //f2 = fragment2.newInstance();
+        //fragment.updateImage(getResources().getDrawable(R.drawable.glas2));
+
+        replaceFragment(f1);
+        //fragment.updateTextView(new Integer(this.counter).toString());
+        // this is the view we will add the gesture detector to
+        View myView = findViewById(R.id.frameLayout);
+
+        // get the gesture detector
+        mDetector = new GestureDetector(this, new SingleCocktailChoice.MyGestureListener());
+
+        // Add a touch listener to the view
+        // The touch listener passes all its events on to the gesture detector
+        myView.setOnTouchListener(touchListener);
+
+    }
+
+
+
+
+    private void replaceFragment(Fragment fragment){
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment,"cocktail");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void replaceFragmentWithOrientation(Fragment fragment, Orientation orientation){
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (orientation){
+            case RIGHT:
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_right,  // enter
+                        R.anim.fade_out  // popExit
+                );
+                break;
+            case LEFT:
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.slide_in_left,  // enter
+                        R.anim.fade_out  // popExit
+                );
+                break;
+            default:
+                fragmentTransaction.setCustomAnimations(
+                        R.anim.fade_in,  // enter
+                        R.anim.fade_out  // popExit
+                );
+        }
+        fragmentTransaction.replace(R.id.frameLayout,fragment,"cocktail");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private List<Recipe> loadRecipes(Context context) throws NotInitializedDBException {
+        List<Recipe> recipes;
+        try {
+            recipes = DatabaseConnection.getDataBase().loadAvailableRecipes();
+        } catch (NotInitializedDBException e) {
+            DatabaseConnection.initialize_singleton(context);
+            recipes = DatabaseConnection.getDataBase().loadAvailableRecipes();
+        }
+        return(recipes);
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/cocktailmachine/ui/fillAnimation/GlassFillFragment.java
+++ app/src/main/java/com/example/cocktailmachine/ui/fillAnimation/GlassFillFragment.java
@@ -1,5 +1,7 @@
 package com.example.cocktailmachine.ui.fillAnimation;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.cocktailmachine.R;
 
@@ -19,48 +23,40 @@ public class GlassFillFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TEXT = "param1";
+    private static final String IMAGE = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String text = "";
+    private Bitmap image = null;
 
-    public GlassFillFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GlassFillFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GlassFillFragment newInstance(String param1, String param2) {
+    ImageView imageView;
+    TextView textView;
+    View view;
+    Context context;
+
+
+    public static GlassFillFragment newInstance(String text, Bitmap image) {
         GlassFillFragment fragment = new GlassFillFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        fragment.text = text;
+        fragment.image = image;
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_glass_fill, container, false);
+        this.view =  inflater.inflate(R.layout.fragment_glass_fill, container, false);
+        this.textView = this.view.findViewById(R.id.textViewCocktail);
+        this.imageView = this.view.findViewById(R.id.imageCocktail);
+        this.imageView.setImageBitmap(this.image);
+        return this.view;
     }
 }
