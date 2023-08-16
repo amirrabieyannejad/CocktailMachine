28fd5b13e4d35effbd4f97a3e72702f3e6042219
Wieberp <Wieberp@students.uni-marburg.de>
2023-08-16 15:54:32 +0200

    Fehleranzeige

---
+++ app/src/main/java/com/example/cocktailmachine/ui/singleCocktailChoice/SingleCocktailChoiceIsNotPossible.java
@@ -0,0 +1,16 @@
+package com.example.cocktailmachine.ui.singleCocktailChoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailmachine.R;
+
+public class SingleCocktailChoiceIsNotPossible extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_cocktail_choice_is_not_possible);
+    }
+}
