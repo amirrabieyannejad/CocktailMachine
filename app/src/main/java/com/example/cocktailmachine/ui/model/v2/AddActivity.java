45a8137b4545b205d3b80ea3a6eb00f8af2ecd37
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-09-11 11:54:09 +0200

    setUptopic

--- app/src/main/java/com/example/cocktailmachine/ui/model/v2/AddActivity.java
+++ app/src/main/java/com/example/cocktailmachine/ui/model/v2/AddActivity.java
@@ -2,15 +2,31 @@ package com.example.cocktailmachine.ui.model.v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.cocktailmachine.R;
+import com.example.cocktailmachine.data.Ingredient;
+import com.example.cocktailmachine.data.Pump;
+import com.example.cocktailmachine.data.Recipe;
+import com.example.cocktailmachine.data.Topic;
 import com.example.cocktailmachine.data.db.DatabaseConnection;
 import com.example.cocktailmachine.databinding.ActivityAddBinding;
 import com.example.cocktailmachine.databinding.ActivityDisplayBinding;
+import com.example.cocktailmachine.ui.model.FragmentType;
+import com.example.cocktailmachine.ui.model.ModelType;
 
 public class AddActivity extends BasicActivity {
     ActivityAddBinding binding;
+    Pump pump;
+    Topic topic;
+    Recipe recipe;
+    Ingredient ingredient;
+
+    final Activity activity = this;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +38,108 @@ public class AddActivity extends BasicActivity {
 
     @Override
     void preSetUp() {
+        binding.editTextAddTitle.setVisibility(View.GONE);
+        binding.switchAlcohol.setVisibility(View.GONE);
+        binding.includeAlcoholic.getRoot().setVisibility(View.GONE);
+        binding.includeNotAlcoholic.getRoot().setVisibility(View.GONE);
+        binding.editTextDescription.setVisibility(View.GONE);
+        binding.textViewAddTitle.setVisibility(View.GONE);
+
+        //for all
+        binding.buttonStop.setOnClickListener(v -> GetActivity.goBack(activity));
 
     }
 
     @Override
     void setUpPump() {
 
+        //nedded
+        binding.textViewAddTitle.setVisibility(View.VISIBLE);
+
+        //maybe needed
+
+
+
+        pump = Pump.getPump(this.getID());
+
     }
 
     @Override
     void setUpTopic() {
+        String title_tag = "Serviervorschlag: ";
+
+        //nedded
+        binding.editTextAddTitle.setVisibility(View.VISIBLE);
+        binding.textViewAddTitle.setVisibility(View.VISIBLE);
+        binding.editTextDescription.setVisibility(View.VISIBLE);
+
+        binding.textViewAddTitle.setText(title_tag);
+
+        topic = Topic.getTopic(this.getID());
+        if(topic == null){
+            binding.buttonSave.setOnClickListener(v -> {
+                topic = Topic.makeNew(binding.editTextAddTitle.getText().toString(), binding.editTextDescription.getText().toString());
+                if(!topic.save()){
+                    error();
+                    return;
+                }
+                GetActivity.goToDisplay(activity, FragmentType.Model, ModelType.TOPIC, topic.getID());
+            });
+
+        }else{
+            binding.editTextAddTitle.setText(topic.getName());
+            binding.editTextDescription.setText(topic.getDescription());
+            binding.buttonSave.setOnClickListener(v -> {
+                topic.setName(binding.editTextAddTitle.getText().toString());
+                topic.setDescription(binding.editTextDescription.getText().toString());
+                if(!topic.save()){
+                    error();
+                    return;
+                }
+                GetActivity.goToDisplay(activity, FragmentType.Model, ModelType.TOPIC, topic.getID());
+            });
+        }
+
+
 
     }
 
     @Override
     void setUpIngredient() {
+        //nedded
+        binding.editTextAddTitle.setVisibility(View.VISIBLE);
+        binding.textViewAddTitle.setVisibility(View.VISIBLE);
+        binding.switchAlcohol.setVisibility(View.VISIBLE);
+
+        //maybe needed
+        binding.includeAlcoholic.getRoot().setVisibility(View.GONE);
+        binding.includeNotAlcoholic.getRoot().setVisibility(View.GONE);
 
     }
 
     @Override
     void setUpRecipe() {
 
+
+        //nedded
+        binding.editTextAddTitle.setVisibility(View.VISIBLE);
+        binding.textViewAddTitle.setVisibility(View.VISIBLE);
+
+
+        //maybe needed
+        binding.includeAlcoholic.getRoot().setVisibility(View.GONE);
+        binding.includeNotAlcoholic.getRoot().setVisibility(View.GONE);
+
     }
 
     @Override
     void postSetUp() {
 
+    }
+
+    void error(){
+        preSetUp();
+        binding.textViewAddTitle.setText("Fehler!");
     }
 
 
