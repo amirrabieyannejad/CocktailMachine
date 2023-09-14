63de2d63c60fb5229af1c4798422e7642d304199
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-14 13:25:46 +0200

    Konfiguration von RecyclerView.Adapter

--- app/src/main/java/com/example/cocktailmachine/ui/ListOfIngredience/RecyclerAdapterListIngredience.java
+++ app/src/main/java/com/example/cocktailmachine/ui/ListOfIngredience/RecyclerAdapterListIngredience.java
@@ -1,14 +1,32 @@
 package com.example.cocktailmachine.ui.ListOfIngredience;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerAdapterListIngredience {
+import com.example.cocktailmachine.R;
+
+public class RecyclerAdapterListIngredience extends RecyclerView.Adapter<RecyclerAdapterListIngredience.IngtedienceViewHolder> {
 
 
+    @NonNull
+    @Override
+    public IngtedienceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull IngtedienceViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public static class IngtedienceViewHolder extends RecyclerView.ViewHolder {
         public TextView ingtedienceName;
