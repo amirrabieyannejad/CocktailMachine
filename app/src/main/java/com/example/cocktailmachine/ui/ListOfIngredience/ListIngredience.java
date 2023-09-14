902892d863d3ae3dfe4f619267a41e5a33e990f7
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-14 14:29:55 +0200

    Anbindung von ListIngredience und behebung von Problemen

--- app/src/main/java/com/example/cocktailmachine/ui/ListOfIngredience/ListIngredience.java
+++ app/src/main/java/com/example/cocktailmachine/ui/ListOfIngredience/ListIngredience.java
@@ -1,16 +1,51 @@
 package com.example.cocktailmachine.ui.ListOfIngredience;
 
+import static com.example.cocktailmachine.data.Topic.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.cocktailmachine.R;
+import com.example.cocktailmachine.data.Ingredient;
+import com.example.cocktailmachine.data.db.DatabaseConnection;
+import com.example.cocktailmachine.data.db.exceptions.NotInitializedDBException;
+import com.example.cocktailmachine.data.enums.AdminRights;
+
+import java.util.List;
 
 public class ListIngredience extends AppCompatActivity {
 
+
+    private List<Ingredient> listIngredients;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_ingredience);
+
+        if(!DatabaseConnection.isInitialized()) {
+            Log.i(TAG, "onCreate: DataBase is not yet initialized");
+            DatabaseConnection.initializeSingleton(this, AdminRights.getUserPrivilegeLevel());// UserPrivilegeLevel.Admin);
+            try {
+                DatabaseConnection.getDataBase();
+                Log.i(TAG, "onCreate: DataBase is initialized");
+                //Log.i(TAG, Recipe.getAllRecipesAsMessage().toString());
+            } catch (NotInitializedDBException e) {
+                e.printStackTrace();
+                Log.e(TAG, "onCreate: DataBase is not initialized");
+            }
+        }else{
+            Log.i(TAG, "onCreate: DataBase is already initialized");
+        }
+
+        try {
+            listIngredients = (List<Ingredient>) DatabaseConnection.getDataBase().getAllIngredients();
+        } catch (NotInitializedDBException e) {
+            e.printStackTrace();
+            Log.e(TAG, "ListIngredience : Error "+ e.toString());
+        }
+
+        System.out.println("");
     }
 }
