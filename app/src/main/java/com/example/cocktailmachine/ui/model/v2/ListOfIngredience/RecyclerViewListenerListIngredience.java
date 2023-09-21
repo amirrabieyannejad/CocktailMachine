cbf7ca1291bdceb44e828943d5df6199a8daff78
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-21 12:41:51 +0200

    Anpassung einzelner Elemente

--- app/src/main/java/com/example/cocktailmachine/ui/model/v2/ListOfIngredience/RecyclerViewListenerListIngredience.java
+++
@@ -1,8 +0,0 @@
-package com.example.cocktailmachine.ui.model.v2.ListOfIngredience;
-
-import com.example.cocktailmachine.data.Ingredient;
-
-public interface RecyclerViewListenerListIngredience {
-
-    void selectIngredience(Ingredient ingredient);
-}
