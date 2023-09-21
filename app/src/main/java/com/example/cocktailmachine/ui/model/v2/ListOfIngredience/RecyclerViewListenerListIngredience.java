c0e93ab50e10407cec62f69664aecb4132b21936
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-21 12:41:27 +0200

    Einfügen der Listenelemente

---
+++ app/src/main/java/com/example/cocktailmachine/ui/model/v2/ListOfIngredience/RecyclerViewListenerListIngredience.java
@@ -0,0 +1,8 @@
+package com.example.cocktailmachine.ui.model.v2.ListOfIngredience;
+
+import com.example.cocktailmachine.data.Ingredient;
+
+public interface RecyclerViewListenerListIngredience {
+
+    void selectIngredience(Ingredient ingredient);
+}
