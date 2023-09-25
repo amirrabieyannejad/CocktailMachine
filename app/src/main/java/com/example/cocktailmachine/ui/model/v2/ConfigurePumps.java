121ea2cc385657c59202b45d76078fd998deca7c
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-25 15:24:09 +0200

    Entfernung nicht mehr benötigter Klassen

--- app/src/main/java/com/example/cocktailmachine/ui/model/v2/ConfigurePumps.java
+++ app/src/main/java/com/example/cocktailmachine/ui/model/v2/ConfigurePumps.java
@@ -4,15 +4,11 @@ import static com.example.cocktailmachine.data.Topic.TAG;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
