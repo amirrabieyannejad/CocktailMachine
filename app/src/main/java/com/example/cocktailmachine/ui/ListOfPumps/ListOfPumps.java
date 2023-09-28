f01aabc819dc7fa4dbdf02d9ef82a0965c68ff1e
Wieberp <Wieberp@students.uni-marburg.de>
2023-09-28 11:56:59 +0200

    Einführung der Activität ListOfPumps

---
+++ app/src/main/java/com/example/cocktailmachine/ui/ListOfPumps/ListOfPumps.java
@@ -0,0 +1,16 @@
+package com.example.cocktailmachine.ui.ListOfPumps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailmachine.R;
+
+public class ListOfPumps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_pumps);
+    }
+}
