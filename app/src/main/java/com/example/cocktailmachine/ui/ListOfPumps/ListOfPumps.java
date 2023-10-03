b6c62749760271d61ba1c490d79b963e2a5eba46
Wieberp <Wieberp@students.uni-marburg.de>
2023-10-03 14:01:23 +0200

    Einrichtung des RecyclerViews in ListOfPumps

--- app/src/main/java/com/example/cocktailmachine/ui/ListOfPumps/ListOfPumps.java
+++ app/src/main/java/com/example/cocktailmachine/ui/ListOfPumps/ListOfPumps.java
@@ -1,21 +1,38 @@
 package com.example.cocktailmachine.ui.ListOfPumps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.cocktailmachine.R;
 import com.example.cocktailmachine.data.Pump;
+import com.example.cocktailmachine.ui.ListOfIngredience.RecyclerAdapterListIngredience;
+
+import java.util.List;
 
 public class ListOfPumps extends AppCompatActivity implements RecyclerViewListenerListPumps {
+
+    private List<Pump> listPumps;
+    private RecyclerView recyclerView;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_pumps);
-        //Pump.setOverrideEmptyPumps(this,5);
-        Toast.makeText(this,Pump.getPumps(this).size(),Toast.LENGTH_LONG).show();
+
+        this.context = this;
+        this.listPumps = Pump.getPumps(this);
+
+        //Einrichtung des RecyclerView
+        recyclerView = findViewById(R.id.recyclerViewActivityListOfPumps);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerAdapterListPumps adapterComments = new RecyclerAdapterListPumps(this.listPumps,this.context);
+        recyclerView.setAdapter(adapterComments);
     }
 
     @Override
