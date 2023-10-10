5f4572e4ec771bbcc0f9b4a85ce508a88657af2e
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-10-10 15:03:39 +0200

    testing

---
+++ app/src/test/java/com/example/cocktailmachine/data/BasicRecipesTest.java
@@ -0,0 +1,37 @@
+package com.example.cocktailmachine.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Johanna Reidt
+ * @created Di. 10.Okt 2023 - 14:52
+ * @project CocktailMachine
+ */
+class BasicRecipesTest {
+
+    @Test
+    void loadTopics() {
+    }
+
+    @Test
+    void loadIngredients() {
+    }
+
+    @Test
+    void loadPumps() {
+    }
+
+    @Test
+    void loadMargarita() {
+    }
+
+    @Test
+    void loadTequila() {
+    }
+
+    @Test
+    void loadLongIslandIceTea() {
+    }
+}
---
+++ app/src/test/java/com/example/cocktailmachine/data/TopicTest.java
@@ -0,0 +1,65 @@
+package com.example.cocktailmachine.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Johanna Reidt
+ * @created Di. 10.Okt 2023 - 14:43
+ * @project CocktailMachine
+ */
+class TopicTest {
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void makeNew() {
+    }
+
+    @Test
+    void getTopic() {
+    }
+
+    @Test
+    void testGetTopic() {
+    }
+
+    @Test
+    void getTopics() {
+    }
+
+    @Test
+    void getTopicIDs() {
+    }
+
+    @Test
+    void testGetTopics() {
+    }
+
+    @Test
+    void testGetTopics1() {
+    }
+
+    @Test
+    void getTopicTitles() {
+    }
+}
