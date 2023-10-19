deb82baf38d89295c41593abe464fb190620b306
Wieberp <Wieberp@students.uni-marburg.de>
2023-10-19 12:41:27 +0200

    Einführung der Klasse BluetoothNotFound.java

---
+++ app/src/main/java/com/example/cocktailmachine/ui/BluetoothNotFound.java
@@ -0,0 +1,16 @@
+package com.example.cocktailmachine.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailmachine.R;
+
+public class BluetoothNotFound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_not_found);
+    }
+}
