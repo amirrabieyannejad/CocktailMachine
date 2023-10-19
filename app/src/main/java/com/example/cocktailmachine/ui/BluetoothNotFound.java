3a6a344c2e254fd3f2aecbadadd9a2f9b3cc2001
Wieberp <Wieberp@students.uni-marburg.de>
2023-10-19 13:34:57 +0200

    Erzeugung eines zufälligen Rezeptes in  BluetoothNotFound.java

--- app/src/main/java/com/example/cocktailmachine/ui/BluetoothNotFound.java
+++ app/src/main/java/com/example/cocktailmachine/ui/BluetoothNotFound.java
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cocktailmachine.R;
+import com.example.cocktailmachine.data.Ingredient;
+import com.example.cocktailmachine.data.Recipe;
+import com.example.cocktailmachine.data.db.elements.SQLIngredient;
+import com.example.cocktailmachine.data.db.elements.SQLRecipe;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 public class BluetoothNotFound extends AppCompatActivity {
 
@@ -12,5 +20,18 @@ public class BluetoothNotFound extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_not_found);
+    }
+
+    private Recipe getRandomRecipe(){
+        Recipe recipe = new SQLRecipe(-1, "random", false, true);
+        Random random = new Random();
+        int numberIngredience = random.nextInt(3)+2;
+        List<Ingredient> list = new LinkedList<>();
+        for (int i = 0; i < numberIngredience; i++) {
+            Ingredient ingredient = new SQLIngredient("ingredient" + i, false, (-1) * random.nextInt(16777216));
+            recipe.add(this, ingredient,random.nextInt(5)+4);
+
+        }
+        return recipe;
     }
 }
