a3fee1128a6375389cc42f1a7fd419687238ff34
Wieberp <Wieberp@students.uni-marburg.de>
2023-10-25 19:55:17 +0200

    Erstellung von SinglePumpSetting.java

---
+++ app/src/main/java/com/example/cocktailmachine/ui/settings/SinglePumpSetting.java
@@ -0,0 +1,16 @@
+package com.example.cocktailmachine.ui.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailmachine.R;
+
+public class SinglePumpSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_pump_setting);
+    }
+}
