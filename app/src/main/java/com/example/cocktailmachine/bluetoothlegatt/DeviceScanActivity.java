04fce1c56f44464ab64f7a49e28ce21419d105b7
Amir Rabieyan Nejad <70313484+amirrabieyannejad@users.noreply.github.com>
2023-11-27 15:09:04 +0100

    update device scan activity

--- app/src/main/java/com/example/cocktailmachine/bluetoothlegatt/DeviceScanActivity.java
+++ app/src/main/java/com/example/cocktailmachine/bluetoothlegatt/DeviceScanActivity.java
@@ -234,8 +234,8 @@ public class DeviceScanActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
         if (device == null) return;
-        //final Intent intent = new Intent(this, BluetoothTestEnviroment.class);
-        Intent intent = new Intent(this, Menue.class);
+        final Intent intent = new Intent(this, BluetoothTestEnviroment.class);
+        //Intent intent = new Intent(this, Menue.class);
         //final Intent intent = new Intent(this, DeviceControlActivity.class);
         BluetoothSingleton settings = BluetoothSingleton.getInstance();
         settings.setEspDeviceName(device.getName());
@@ -256,8 +256,8 @@ public class DeviceScanActivity extends ListActivity {
                     Toast.LENGTH_SHORT).show();
             return;
         }
-        //final Intent intent = new Intent(this, BluetoothTestEnviroment.class);
-        Intent intent = new Intent(this, Menue.class);
+        final Intent intent = new Intent(this, BluetoothTestEnviroment.class);
+        //Intent intent = new Intent(this, Menue.class);
         //final Intent intent = new Intent(this, DeviceControlActivity.class);
         BluetoothSingleton settings = BluetoothSingleton.getInstance();
         settings.setEspDeviceName(device.getName());
