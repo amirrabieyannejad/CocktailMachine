4cfe0a100a6fb28655d5ee7867f05c40ae8d2211
johannareidt <56403676+johannareidt@users.noreply.github.com>
2023-12-14 15:30:59 +0100

    WaitNotSet direkt kalibrierung

--- app/src/main/java/com/example/cocktailmachine/ui/model/WaitNotSetActivity.java
+++ app/src/main/java/com/example/cocktailmachine/ui/model/WaitNotSetActivity.java
@@ -23,6 +23,10 @@ public class WaitNotSetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait_not_set);
+
+        if(AdminRights.isAdmin()){
+            GetDialog.startAutomaticCalibration(this);
+        }
     }
 
     public void login(View view) {
