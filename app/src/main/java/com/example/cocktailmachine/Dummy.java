fb510366ef7542be2d0b4550956bfee6f90c8ef4
Wieberp <Wieberp@students.uni-marburg.de>
2024-02-26 17:20:35 +0100

    Merge branch 'main' of https://github.com/johannareidt/CocktailMachine # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- app/src/main/java/com/example/cocktailmachine/Dummy.java
+++ app/src/main/java/com/example/cocktailmachine/Dummy.java
@@ -2,10 +2,10 @@ package com.example.cocktailmachine;
 
 public class Dummy {
     // APP: zur Abgabe: isDummy = false, withSetCalibration=true, withTestEnvs= false, asAdmin = false
-    public final static boolean isDummy = true;
+    public final static boolean isDummy = false;
     //if true-> keine Bluetoothverbindung, für VM-testing,
     // false -> Bluetooth
-    public static boolean withSetCalibration =  false;
+    public static boolean withSetCalibration =  true;
     //false -> keine Kalibrierungscheck,
     // true -> kalibrierungscheck  und ggf. automatische Kalibrierung,
 
@@ -13,7 +13,7 @@ public class Dummy {
     //false -> keine Test Activities
     //true -> Test Activities zugänglich
 
-    public final static boolean asAdmin = true;
+    public final static boolean asAdmin = false;
     //true  -> direkt admin
     //false -> normaler User
 
